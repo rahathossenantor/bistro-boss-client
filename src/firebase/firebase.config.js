79c644370e03569823a90295fdb41988fb1b7171
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-11-10 13:09:07 +0600

    Initial project setup completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW-L8m9pjFUrs_NcFBwgF5uqTNyrvLTZg",
+    authDomain: "bistro1o1.firebaseapp.com",
+    projectId: "bistro1o1",
+    storageBucket: "bistro1o1.appspot.com",
+    messagingSenderId: "1056562299984",
+    appId: "1:1056562299984:web:4d7b77eb0d55176981bd55"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
